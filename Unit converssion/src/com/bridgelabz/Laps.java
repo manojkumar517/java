0d16346539b827857d481579511e487dcb6673e1
manojkumar517 <amanojkumar517@gmail.com>
2022-12-15 20:06:08 +0530

    Added Laps programme

---
+++ Unit converssion/src/com/bridgelabz/Laps.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class Laps {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = a*8;
+        System.out.print("Total laps : " + b);
+    }
+}
